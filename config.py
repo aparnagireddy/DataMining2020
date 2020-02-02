bf710b438aa71a1d0f11db8f685f1a42a61d466d
aparnagireddy <52471132+aparnagireddy@users.noreply.github.com>
2020-02-02 00:28:11 -0500

    initial commits

---
+++ config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "30mDruKOIk4oZUDl3foiUYPd7"
+consumer_secret = "HrTPGccHa6nuZVy1a0yQzke2JHyMGIU2QqifdRJGy2NgyXVxHu"
+access_key = "969650129312641024-HiqIeEn41AEoTdgL95LlogE0bv27nFw"
+access_secret = "UkOuNHrKLCG6p42vMjGgwdZxiEOuHiWkxiM6TMoYA1BxC"
